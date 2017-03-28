3c872dfd61c7fcc2f95c318008b9370b93c937c8
Ryley Sill <ryleysill93@gmail.com>
2017-03-28 16:20:41 -0700

    fixed bundle issues

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   entry: './frontend/musical_tiles.jsx',
   output: {
-    filename: '/docs/bundle.js',
+    filename: './bundle.js',
   },
   module: {
     loaders: [

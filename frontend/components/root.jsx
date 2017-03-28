92e15a142591a8cdc1b3c119cceb9ec8a72f3156
Ryley Sill <ryleysill93@gmail.com>
2017-03-28 15:42:00 -0700

    changed name to musical squares

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -6,7 +6,7 @@ class Root extends React.Component {
   render () {
     return (
       <div >
-        <h1>Musical Tiles</h1>
+        <h1>Musical Squares</h1>
         <Grid />
       </div>
     );
